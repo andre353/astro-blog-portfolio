0710054f1dd4d84a6dd9f1e358681019c5ab5bc4
andre353 <eva595eva@gmail.com>
2023-03-26 10:35:30 +0300

    Add RelatedPosts component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
