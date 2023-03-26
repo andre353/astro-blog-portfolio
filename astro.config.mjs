70c5bf9ecbabf1340013b46a628e52ac3de45952
andre353 <eva595eva@gmail.com>
2023-03-26 15:00:12 +0300

    Add some updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-cip.netlify.app', // rss needs this field
+  site: 'https://astro-blog-cip.netlify.app',
+  // rss needs this field
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }), sitemap(), react(), partytown()]
 });
